10fdfb61f18d673ae648e1245855df1d4df3bdcf
Amrit Bhattacharya <116283061+amrit03b@users.noreply.github.com>
2025-05-21 03:33:58 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     avalancheFuji: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
-      accounts: ["0b1ac645e1f9d47a781b38bed923b06a8470658347c4ddba02d49eccdeffdcb9"]
+      accounts: ["Your Account Number"]
     }
   }
 };
